9b7d6daa9f8dc1a9d99f4f2075035a4be8e2119a
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-12-04 04:41:48 -0300

    archivo de configuracion para manejar las CORS policies del navegador

---
+++ src/main/java/org/humanas/guia/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package org.humanas.guia.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer { //agregamos este archivo de configuracion para lidiar con CORS
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Permite este origen, el frontend deberia correr en esta direccion para desarrollo
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Headers permitidos
+                .allowCredentials(true); // Permite cookies si es necesario
+    }
+}
--- src/main/java/org/humanas/guia/controllers/MajorController.java
+++ src/main/java/org/humanas/guia/controllers/MajorController.java
@@ -5,6 +5,7 @@ import org.humanas.guia.entities.Major;
 import org.humanas.guia.helpers.MockedData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
